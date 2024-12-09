4622ab6b5ad2527c78c1c16b523311442eaaf0c3
lwam-123 <lwamteklay4@gmail.com>
2024-12-08 23:34:59 -0800

    welcome here

---
+++ my first one.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello evry one welcome to my repositry");
+    }
+    
+    public static void Lwam() {
+        System.out.println("i am here");
+    }
+}
